089708fc51b6109fed07796031b630e114d0b006
Michal Margiel <Michal.Margiel@gmail.com>
2019-06-09 23:48:06 +0200

    move to vue-material styling fixes

--- src/views/participant/ParticipatePage.vue
+++ src/views/participant/ParticipatePage.vue
@@ -11,74 +11,59 @@
             <!--                    <span v-else>unknown error {{error}}</span>-->
             <!--                </span>-->
             <!--            </p>-->
-            <div class="back-office">
+            <div>
                 <h4 class="participate__info">All fields required</h4>
                 <h2 class="participate__title">Personal information</h2>
                 <div class="row">
                     <form @submit="save" class="col s12" novalidate>
                         <div class="row">
-                            <div class="input-field col s12">
-                                <input class="" id="first_name" required
-                                       type="text" v-model="form.firstName">
-                                <label for="first_name">first name</label>
-                                <span class="errors helper-text" v-for="error in errors.name">{{error}}</span>
-                            </div>
+                            <md-field>
+                                <label>first name</label>
+                                <md-input v-model="form.firstName"></md-input>
+                            </md-field>
                         </div>
                         <div class="row">
-                            <div class="input-field col s12">
-                                <input class="" id="last_name" required
-                                       type="text" v-model="form.lastName">
-                                <label for="last_name">last name</label>
-                                <span class="errors helper-text" v-for="error in errors.name">{{error}}</span>
-                            </div>
+                            <md-field>
+                                <label>last name</label>
+                                <md-input v-model="form.lastName"></md-input>
+                            </md-field>
                         </div>
                         <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" required
-                                       type="email" v-model="form.email">
-                                <label for="email">e-mail address</label>
-                                <span class="errors helper-text" v-for="error in errors.email">{{error}}</span>
-                            </div>
+                            <md-field>
+                                <label>e-mail address</label>
+                                <md-input v-model="form.email"></md-input>
+                            </md-field>
                         </div>
                         <div class="row">
                             <label class="tshirtCut__label">t-shirt cut</label>
                             <div class="input-field col s12">
-                                <div class="tshirtCut__select">
-                                    <label class="tshirtCut__selectItem">
-                                        <input
-                                                name="t-shirt-cut" type="radio" value="m" v-model="form.gender"/>
-                                        <span>Male</span>
-                                    </label>
-                                    <label class="tshirtCut__selectItem">
-                                        <input class="tshirtCut__selectItem"
-                                               name="t-shirt-cut" type="radio" value="f" v-model="form.gender"/>
-                                        <span>female</span>
-                                    </label>
-                                </div>
+                                <md-radio v-model="form.gender" value="m">male</md-radio>
+                                <md-radio v-model="form.gender" value="f">female</md-radio>
                             </div>
                         </div>
                         <div class="row">
                             <div class="input-field col s12">
-                                <select v-model="form.size">
-                                    <option disabled selected value=""></option>
-                                    <option value="S">S</option>
-                                    <option value="M">M</option>
-                                    <option value="L">L</option>
-                                    <option value="XL">XL</option>
-                                    <option value="XXL">XXL</option>
-                                </select>
-                                <label>T-shirt size</label>
+                                <md-field>
+                                    <label for="t-shirt-size">t-shirt size</label>
+                                    <md-select v-model="form.size" name="t-shirt-size" id="t-shirt-size" required>
+                                        <md-option value="S">S</md-option>
+                                        <md-option value="M">M</md-option>
+                                        <md-option value="L">L</md-option>
+                                        <md-option value="XL">XL</md-option>
+                                        <md-option value="XXL">XXL</md-option>
+                                    </md-select>
+                                </md-field>
                             </div>
                         </div>
 
                         <div class="row">
                             <div class="col s12">
-                                <label>
-                                    <input id="privacyPolicyAccepted" required
-                                           type="checkbox" v-model="form.privacyPolicyAccepted">
-                                    <span>I accept the <router-link
-                                            to="privacy-policy">privacy policy</router-link></span>
-                                </label>
+                                <md-checkbox v-model="form.privacyPolicyAccepted" required>I accept the
+                                    <router-link
+                                            to="privacy-policy">privacy policy
+                                    </router-link>
+                                </md-checkbox>
+
                                 <div class="errors helper-text" v-for="error in errors.privacyPolicyAccepted">
                                     {{error}}
                                 </div>
@@ -96,76 +81,45 @@
                         </div>
 
                         <div class="row">
-
+                            <label class="tshirtCut__label">meal option</label>
                             <div class="input-field col s12">
-                                <select id="mealOption" name="mealOption">
-                                    <option disabled selected value="">select</option>
-                                    <option value="meat">Meat</option>
-                                    <option value="vegetarian">Vegetarian</option>
-                                </select>
-                                <label>meal option</label>
-
-                                <!--                            <div class="helper-text">-->
-                                <!--                                We can't promise that you will be able to get option that you have selected. The food at-->
-                                <!--                                configtura will not be portioned, so first come - first served.-->
-                                <!--                            </div>-->
-                                <!--<mat-error *ngIf="mealOption?.hasError('required')">
-                                    This field is required
-                                </mat-error>-->
+                                <md-radio v-model="form.mealOption" value="meat">meat</md-radio>
+                                <md-radio v-model="form.mealOption" value="vegetarian">vegetarian</md-radio>
                             </div>
+                            <span class="md-helper-text">Food note: We can't guarantee that you will be able to get option that you have selected. The food at confitura will not be portioned, so first come - first served.</span>
                         </div>
                         <div class="row">
-
-                            <div class="input-field col s12">
-                                <input class="" id="city" required
-                                       maxlength="255" name="city"
-                                       type="text" v-model="form.city">
-                                <label for="city">city</label>
-                                <!--  <mat-error *ngIf="city?.hasError('required')">
-                                      This field is required
-                                  </mat-error>-->
-                            </div>
+                            <md-field>
+                                <label>city</label>
+                                <md-input v-model="form.city" maxlength="255" required></md-input>
+                            </md-field>
                         </div>
                         <div class="row">
-
-                            <div class="input-field col s12">
-                                <select id="experience" name="experience"
-                                        required>
-                                    <option disabled selected value=""></option>
-                                    <option value="I am just learning">I am just learning</option>
-                                    <option value="Less than a year">Less than a year</option>
-                                    <option value="1-2 years">1-2 years</option>
-                                    <option value="2-4 years">2-4 years</option>
-                                    <option value="4-8 years">4-8 years</option>
-                                    <option value="Over 8 years">Over 8 years</option>
-                                </select>
-                                <label>experience</label>
-
-                                <!-- <mat-error *ngIf="experience?.hasError('required')">
-                                     This field is required
-                                 </mat-error>-->
-                            </div>
+                            <md-field>
+                                <label for="experience">experience</label>
+                                <md-select v-model="form.experience" name="experience" id="experience" required>
+                                    <md-option value="I am just learning">I am just learning</md-option>
+                                    <md-option value="Less than a year">Less than a year</md-option>
+                                    <md-option value="1-2 years">1-2 years</md-option>
+                                    <md-option value="2-4 years">2-4 years</md-option>
+                                    <md-option value="4-8 years">4-8 years</md-option>
+                                    <md-option value="Over 8 years">Over 8 years</md-option>
+                                </md-select>
+                            </md-field>
                         </div>
                         <div class="row">
-
-                            <div class="input-field col s12">
-                                <select id="role" name="role" placeholder="your role" required>
-                                    <option disabled selected value=""></option>
-
-                                    <option value="student">Student</option>
-                                    <option value="Developer">Developer</option>
-                                    <option value="Team Leader">Team Leader</option>
-                                    <option value="Project Leader">Project Leader</option>
-                                    <option value="Architect">Architect</option>
-                                    <option value="Tester">Tester</option>
-                                    <option value="Other">Other</option>
-                                </select>
-                                <label>your role</label>
-
-                                <!-- <mat-error *ngIf="role?.hasError('required')">
-                                     This field is required
-                                 </mat-error>-->
-                            </div>
+                            <md-field>
+                                <label for="role">role</label>
+                                <md-select v-model="form.role" name="role" id="role" required>
+                                    <md-option value="student">Student</md-option>
+                                    <md-option value="Developer">Developer</md-option>
+                                    <md-option value="Team Leader">Team Leader</md-option>
+                                    <md-option value="Project Leader">Project Leader</md-option>
+                                    <md-option value="Architect">Architect</md-option>
+                                    <md-option value="Tester">Tester</md-option>
+                                    <md-option value="Other">Other</md-option>
+                                </md-select>
+                            </md-field>
                         </div>
                         <div class="row">
 
@@ -174,12 +128,7 @@
                         </span>
                         </div>
                         <div class="row">
-
-                            <div>
-                                <button class="btn waves-effect waves-light button button--save" name="action"
-                                        type="submit">Save
-                                </button>
-                            </div>
+                            <md-button type="submit" class="md-raised md-accent">Save</md-button>
                         </div>
                     </form>
                 </div>
@@ -193,12 +142,19 @@
   import { Component, Vue } from 'vue-property-decorator';
   import Box from '@/components/Box.vue';
   import TheContact from '@/components/TheContact.vue';
-  import M from 'materialize-css';
   import axios from 'axios';
   import PageHeader from '@/components/PageHeader.vue';
   import { Participant, RegistrationForm } from '@/types';
   import { validEmail } from '@/validation-utils';
+  import { MdButton, MdCheckbox, MdField, MdList, MdMenu, MdRadio } from 'vue-material/dist/components';
+  import 'vue-material/dist/vue-material.min.css';
 
+  Vue.use(MdButton);
+  Vue.use(MdField);
+  Vue.use(MdRadio);
+  Vue.use(MdMenu);
+  Vue.use(MdList);
+  Vue.use(MdCheckbox);
   @Component({
     components: { PageHeader, Box, TheContact },
   })
@@ -207,22 +163,22 @@
     public errors: any = {};
 
     public mounted() {
-      M.AutoInit();
+      // M.AutoInit();
       const { voucher } = this.$route.query;
       axios.get(`/api/vouchers/${voucher}/check`).then((value) => {
         this.form = { voucher: { id: voucher as string } };
-        setTimeout(() => {
-          M.updateTextFields();
-          M.FormSelect.init(document.querySelectorAll('select'));
-        });
+        // setTimeout(() => {
+        //   M.updateTextFields();
+        //   M.FormSelect.init(document.querySelectorAll('select'));
+        // });
       }, (reason) => {
         this.errors = { voucher: [reason.response.data] };
       });
     }
 
-    public updated() {
-      M.updateTextFields();
-    }
+    // public updated() {
+    //   M.updateTextFields();
+    // }
 
     public save(event: Event) {
       event.preventDefault();
@@ -289,6 +245,7 @@
         color: $brand;
         font-weight: bold;
         font-size: 2.5rem;
+        line-height: 2.7rem;
     }
 
     .tshirtCut__label {
