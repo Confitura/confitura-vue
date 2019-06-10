e248b8c7429ca1f7f51049423aa2bf99aae9869f
Michal Margiel <Michal.Margiel@gmail.com>
2019-06-11 00:17:28 +0200

    style changes in registration flow

--- src/views/participant/ParticipantPage.vue
+++ src/views/participant/ParticipantPage.vue
@@ -1,56 +1,85 @@
 <template>
     <div class="participant">
-        <PageHeader :small="true" title="See you on Saturday!"></PageHeader>
-        <Box :full="false" class="content " color="white">
-            <div v-if="participant">
-                <p>
-                    Congrats {{participant.firstName}} {{participant.lastName}} !!! <br/>
-                    You are all setup to come to conference! <br/>
-                    Create account and you will be able to create personal agenda and also rate presentation after <br/>
-                    seeing them :)
-                </p>
-                <p>
-                    Here is your ticket. Show it at the registration desk and enjoy the conference!
-                    <img :src="qrcode" alt="b0999fbb-408c-4442-93fb-5b81e8a3265a"
-                         v-if="qrcode"/>
-                </p>
-                <p>
-                    You will also recieve the ticket via email so don't worry if you loose this address :)
-                </p>
+        <PageHeader  title="Registration" type="peace"></PageHeader>
+        <Box :full="false" color="white">
+            <div v-if="participant" class="confirmation">
+                <h2 class="header">Success!</h2>
+                <div class="message">
+                    <p>
+                        Congrats {{participant.firstName}} {{participant.lastName}} !!! <br/>
+                        You are all setup to come to Confitura 2019!
+                    </p>
+                    <p>Please consider create an account and you will be able to create personal agenda and also rate presentation during the conference.
+                    Click <router-link to="/login">here</router-link> to do so.</p>
+                    <p>
+                        Here is your ticket. Show it at the registration desk and enjoy the conference!
+                        <img :src="qrcode" alt="b0999fbb-408c-4442-93fb-5b81e8a3265a"
+                             v-if="qrcode"/>
+                    </p>
+                    <p>
+                        You will also receive it on your email address so don't worry if you loose this address :)
+                    </p></div>
             </div>
         </Box>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Box from '@/components/Box.vue';
-import TheContact from '@/components/TheContact.vue';
-import axios from 'axios';
-import PageHeader from '@/components/PageHeader.vue';
-import { Participant, PARTICIPATION_ID } from '@/types';
+  import { Component, Vue } from 'vue-property-decorator';
+  import Box from '@/components/Box.vue';
+  import TheContact from '@/components/TheContact.vue';
+  import axios from 'axios';
+  import PageHeader from '@/components/PageHeader.vue';
+  import { Participant, PARTICIPATION_ID } from '@/types';
 
-@Component({
-  components: { PageHeader, Box, TheContact },
-})
-export default class ParticipantPage extends Vue {
+  @Component({
+    components: { PageHeader, Box, TheContact },
+  })
+  export default class ParticipantPage extends Vue {
 
-  public participant: Participant | null = null;
-  public qrcode: string = '';
+    public participant: Participant | null = null;
+    public qrcode: string = '';
 
-  public mounted() {
-    const { id } = this.$route.params;
-    axios.get<Participant>(`/api/participants/${id}`)
-      .then((it) => {
-        localStorage.setItem(PARTICIPATION_ID, id);
-        this.participant = it.data;
-        this.qrcode = `/api/participants/${this.participant.id}/ticket`;
-      });
+    public mounted() {
+      const { id } = this.$route.params;
+      axios.get<Participant>(`/api/participants/${id}`)
+        .then((it) => {
+          localStorage.setItem(PARTICIPATION_ID, id);
+          this.participant = it.data;
+          this.qrcode = `/api/participants/${this.participant.id}/ticket`;
+        });
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
+    @import "../../assets/colors";
+    @import "../../assets/sizes";
+    @import "../../assets/media";
+    @import "../../assets/fonts";
+    .confirmation {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        @include md(){
+            flex-direction: row;
+        }
+    }
 
+    .header {
+        color: $brand;
+        font-size: 3rem;
+        font-weight: bold;
+        flex-basis: 30%;
+        margin-right: 3rem;
+    }
+
+    .message {
+        flex-basis: 50%;
+        font-size: 1.3rem;
+        line-height: 1.5rem;
+
+
+    }
 
 </style>
--- src/views/participant/ParticipatePage.vue
+++ src/views/participant/ParticipatePage.vue
@@ -1,76 +1,69 @@
 <template>
     <div class="particiate">
-        <PageHeader title="Registration form" type="peace"></PageHeader>
-        <Box class="content " color="white">
-            <!--            <p v-if="!form">-->
-            <!--                Unable to register as participant:-->
-            <!--                <span v-for="error in errors.voucher">-->
-            <!--                    <br/>-->
-            <!--                    <span v-if="error === 'INVALID'">invalid voucher</span>-->
-            <!--                    <span v-if="error === 'TAKEN'">voucher already used</span>-->
-            <!--                    <span v-else>unknown error {{error}}</span>-->
-            <!--                </span>-->
-            <!--            </p>-->
-            <div>
+        <PageHeader title="Registration" type="peace"></PageHeader>
+        <Box class="content " color="white" :full="false">
+            <p v-if="!form" class="fatalError">
+                <strong>Unable to register as participant: </strong>
+                <span v-if="loadError === 'INVALID'">invalid voucher</span>
+                <span v-if="loadError === 'TAKEN'">voucher already used</span>
+                <span v-else>unknown error {{loadError}}</span>
+            </p>
+            <div v-else>
                 <h4 class="participate__info">All fields required</h4>
                 <h2 class="participate__title">Personal information</h2>
                 <div class="row">
                     <form @submit="save" class="col s12" novalidate>
                         <div class="row">
-                            <md-field>
+                            <md-field :class="{'md-invalid': errors.has('firstName')}">
                                 <label>first name</label>
-                                <md-input v-model="form.firstName"></md-input>
+                                <md-input v-model="form.firstName" required v-validate="'required'" name="firstName"></md-input>
+                                <span class="md-error">{{ errors.first('firstName') }}</span>
                             </md-field>
                         </div>
                         <div class="row">
-                            <md-field>
+                            <md-field :class="{'md-invalid': errors.has('last name')}">
                                 <label>last name</label>
-                                <md-input v-model="form.lastName"></md-input>
+                                <md-input v-model="form.lastName" required v-validate="'required'" name="last name"></md-input>
+                                <span class="md-error">{{ errors.first('last name') }}</span>
                             </md-field>
                         </div>
                         <div class="row">
-                            <md-field>
+                            <md-field :class="{'md-invalid': errors.has('e-mail address')}">
                                 <label>e-mail address</label>
-                                <md-input v-model="form.email"></md-input>
+                                <md-input type="email" v-model="form.email" required v-validate="'required|email'"
+                                          name="e-mail address"></md-input>
+                                <span class="md-error">{{ errors.first('e-mail address') }}</span>
+
                             </md-field>
                         </div>
                         <div class="row">
-                            <label class="tshirtCut__label">t-shirt cut</label>
-                            <div class="input-field col s12">
-                                <md-radio v-model="form.gender" value="m">male</md-radio>
-                                <md-radio v-model="form.gender" value="f">female</md-radio>
-                            </div>
+                            <md-field :class="{'md-invalid': errors.has('t-shirt cut')}">
+                                <label for="t-shirt-cut">t-shirt size</label>
+                                <md-select v-model="form.gender" name="t-shirt cut" id="t-shirt-cut" required
+                                           v-validate="'required'">
+                                    <md-option value="m">male</md-option>
+                                    <md-option value="f">female</md-option>
+                                </md-select>
+                                <span class="md-error">{{ errors.first('t-shirt cut') }}</span>
+                            </md-field>
                         </div>
                         <div class="row">
                             <div class="input-field col s12">
-                                <md-field>
+                                <md-field :class="{'md-invalid': errors.has('t-shirt size')}">
                                     <label for="t-shirt-size">t-shirt size</label>
-                                    <md-select v-model="form.size" name="t-shirt-size" id="t-shirt-size" required>
+                                    <md-select v-model="form.size" name="t-shirt size" id="t-shirt-size" required
+                                               v-validate="'required'">
                                         <md-option value="S">S</md-option>
                                         <md-option value="M">M</md-option>
                                         <md-option value="L">L</md-option>
                                         <md-option value="XL">XL</md-option>
                                         <md-option value="XXL">XXL</md-option>
                                     </md-select>
+                                    <span class="md-error">{{ errors.first('t-shirt size') }}</span>
+
                                 </md-field>
                             </div>
                         </div>
-
-                        <div class="row">
-                            <div class="col s12">
-                                <md-checkbox v-model="form.privacyPolicyAccepted" required>I accept the
-                                    <router-link
-                                            to="privacy-policy">privacy policy
-                                    </router-link>
-                                </md-checkbox>
-
-                                <div class="errors helper-text" v-for="error in errors.privacyPolicyAccepted">
-                                    {{error}}
-                                </div>
-
-                            </div>
-                        </div>
-
                         <div class="row">
                             <h2 class="participate__title">Tell us more about you</h2>
                             <div class="participate__statisticsInfo">This data will be used for statistics only, and will not be associated
@@ -81,23 +74,34 @@
                         </div>
 
                         <div class="row">
-                            <label class="tshirtCut__label">meal option</label>
-                            <div class="input-field col s12">
-                                <md-radio v-model="form.mealOption" value="meat">meat</md-radio>
-                                <md-radio v-model="form.mealOption" value="vegetarian">vegetarian</md-radio>
-                            </div>
-                            <span class="md-helper-text">Food note: We can't guarantee that you will be able to get option that you have selected. The food at confitura will not be portioned, so first come - first served.</span>
+                            <md-field :class="{'md-invalid': errors.has('meal option')}">
+                                <label for="meal-option">meal option</label>
+                                <md-select v-model="form.mealOption" name="meal option" id="meal-option" required
+                                           v-validate="'required'">
+                                    <md-option value="meat">meat</md-option>
+                                    <md-option value="vegetarian">vegetarian</md-option>
+                                </md-select>
+
+                                <span class="md-error">{{ errors.first('meal option') }}</span>
+                            </md-field>
+
                         </div>
                         <div class="row">
-                            <md-field>
+                            <strong>Food note:</strong> We can't guarantee that you will be able to get option that you have selected. The
+                            food at confitura will not be portioned, so first come - first served.
+                        </div>
+                        <div class="row">
+                            <md-field :class="{'md-invalid': errors.has('city')}">
                                 <label>city</label>
-                                <md-input v-model="form.city" maxlength="255" required></md-input>
+                                <md-input v-model="form.city" maxlength="255" required v-validate="'required'" name="city"></md-input>
+                                <span class="md-error">{{ errors.first('city') }}</span>
                             </md-field>
                         </div>
                         <div class="row">
-                            <md-field>
+                            <md-field :class="{'md-invalid': errors.has('experience')}">
                                 <label for="experience">experience</label>
-                                <md-select v-model="form.experience" name="experience" id="experience" required>
+                                <md-select v-model="form.experience" name="experience" id="experience" required
+                                           v-validate="'required'">
                                     <md-option value="I am just learning">I am just learning</md-option>
                                     <md-option value="Less than a year">Less than a year</md-option>
                                     <md-option value="1-2 years">1-2 years</md-option>
@@ -105,12 +109,14 @@
                                     <md-option value="4-8 years">4-8 years</md-option>
                                     <md-option value="Over 8 years">Over 8 years</md-option>
                                 </md-select>
+                                <span class="md-error">{{ errors.first('experience') }}</span>
                             </md-field>
                         </div>
                         <div class="row">
-                            <md-field>
+                            <md-field :class="{'md-invalid': errors.has('experience')}">
                                 <label for="role">role</label>
-                                <md-select v-model="form.role" name="role" id="role" required>
+                                <md-select v-model="form.role" name="role" id="role" required
+                                           v-validate="'required'">
                                     <md-option value="student">Student</md-option>
                                     <md-option value="Developer">Developer</md-option>
                                     <md-option value="Team Leader">Team Leader</md-option>
@@ -119,13 +125,16 @@
                                     <md-option value="Tester">Tester</md-option>
                                     <md-option value="Other">Other</md-option>
                                 </md-select>
+                                <span class="md-error">{{ errors.first('role') }}</span>
+
                             </md-field>
                         </div>
                         <div class="row">
-
-                        <span v-for="error in errors.form">
-                             {{error}} <br/>
-                        </span>
+                            <md-checkbox v-model="form.privacyPolicyAccepted" required v-validate="'required'" name="privacy policy">I
+                                accept the
+                                <router-link to="privacy-policy">privacy policy</router-link>
+                            </md-checkbox>
+                            <div class="error" v-if="errors.has('privacy policy')">Privacy Policy has to be accepted</div>
                         </div>
                         <div class="row">
                             <md-button type="submit" class="md-raised md-accent">Save</md-button>
@@ -145,7 +154,6 @@
   import axios from 'axios';
   import PageHeader from '@/components/PageHeader.vue';
   import { Participant, RegistrationForm } from '@/types';
-  import { validEmail } from '@/validation-utils';
   import { MdButton, MdCheckbox, MdField, MdList, MdMenu, MdRadio } from 'vue-material/dist/components';
   import 'vue-material/dist/vue-material.min.css';
 
@@ -159,65 +167,30 @@
     components: { PageHeader, Box, TheContact },
   })
   export default class ParticipatePage extends Vue {
-    public form: RegistrationForm = { voucher: { id: 'abc' } };
-    public errors: any = {};
+    public form: RegistrationForm | null = null;
+    public loadError: string = '';
 
     public mounted() {
-      // M.AutoInit();
       const { voucher } = this.$route.query;
-      axios.get(`/api/vouchers/${voucher}/check`).then((value) => {
-        this.form = { voucher: { id: voucher as string } };
-        // setTimeout(() => {
-        //   M.updateTextFields();
-        //   M.FormSelect.init(document.querySelectorAll('select'));
-        // });
-      }, (reason) => {
-        this.errors = { voucher: [reason.response.data] };
-      });
+      axios.get(`/api/vouchers/${voucher}/check`)
+        .then(() => {
+          this.form = { voucher: { id: voucher as string } };
+        }, (reason) => {
+          this.loadError = reason.response.data;
+        });
     }
 
-    // public updated() {
-    //   M.updateTextFields();
-    // }
 
-    public save(event: Event) {
+    public async save(event: Event) {
       event.preventDefault();
-      if (this.validate()) {
-        // do save
+      const isValid = await this.$validator.validate();
+      if (isValid) {
         axios.post<Participant>('/api/participants', this.form)
           .then((it) => {
             const id: string = it.data.id as string;
             this.$router.push({ name: 'participant', params: { id } });
           });
       }
-    }
-
-
-    private validate() {
-      const errors: any = {};
-      if (this.form == null) {
-        return false;
-      }
-      let valid = true;
-
-      if (!this.form.email || !validEmail(this.form.email)) {
-        errors.email = ['invalid email'];
-        valid = false;
-      }
-      if (!this.form.firstName) {
-        errors.name = ['Name is required'];
-        valid = false;
-      }
-      if (!this.form.lastName) {
-        errors.name = ['Last name is required'];
-        valid = false;
-      }
-      if (!this.form.privacyPolicyAccepted) {
-        errors.privacyPolicyAccepted = ['Agreeing to our policy is required'];
-        valid = false;
-      }
-      this.errors = errors;
-      return valid;
     }
   }
 </script>
@@ -268,6 +241,15 @@
     .participate__statisticsInfo {
         font-size: 1.2rem;
         line-height: 1.4rem;
+    }
+
+    .error {
+        color: #ff1744;
+        font-size: 12px;
+    }
+    .fatalError {
+        color: #ff1744;
+
     }
 
 
